463a6d16bd3788c4828960b7ff486e3a0b6649ee
nabilainas <ainaspro@gmail.com>
2023-07-08 14:52:43 +0200

    Update README file

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ msg['To'] = receiver
 
 
 msg.set_content("Test email on local server ...")
+
+port = 1025
+
+with smtplib.SMTP('localhost', port ) as server:
+    server.send_message(msg)
+    print("Test email sent successfully")
