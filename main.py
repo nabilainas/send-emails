75a399d7647262efb89b68f5b93cec51a8f80a08
nabilainas <ainaspro@gmail.com>
2023-07-08 04:06:59 +0200

    Update main

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import smtplib
 from email.message import EmailMessage
+
+
+subject = "TEST subject"
+sender = "nabil@aineed.com"
+receiver = "hakim@aineed.com"
+
+msg = EmailMessage()
+
+msg['Subject'] = subject
+msg['From'] = sender
+msg['To'] = receiver
+
+
+msg.set_content("Test email on local server ...")
